08d02cc307e21ecf3e72773ec3475dc86fe9a56e
Akhilesh Yadav <akhileshyadavkokta@gmail.com>
2020-11-30 19:58:53 +0530

    make it better

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-
+SECRET_KEY= '5b=!2mz4*-@zlhkt_^d&5g&&w9@o0%jjheydx5$)mfs6e61fjh'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
